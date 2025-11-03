101a7092ff2a162515557017ceb37a9d88992da1
André Gomes <netofsa2012@gmail.com>
2025-11-02 21:22:05 -0300

    Delete tialudeliveryapp directory

--- tialudeliveryapp/model/Order.kt
+++
@@ -1,9 +0,0 @@
-package br.com.fooddelivery.tialudeliveryapp.model
-
-data class Order(
-    val id: Int,
-    val customerName: String,
-    val status: String,
-    val total: Double,
-    val date: String
-)
